6cfdb0584045c705eafe2711be7f33db1bb016b7
Ryan Conrad <camalot@gmail.com>
2019-09-14 17:16:25 -0500

    package

---
+++ Build/appveyor.before-package.ps1
@@ -0,0 +1,16 @@
+if ( !$env:APPVEYOR_PULL_REQUEST_NUMBER -and ($env:APPVEYOR_REPO_BRANCH -eq "develop") ) {
+  $env:CI_DEPLOY_GITHUB = $true;
+
+	if ( !(Test-Path -Path "../deps/$ENV:APPVEYOR_PROJECT_SLUG" ) ) {
+			New-Item -Path "../deps/$ENV:APPVEYOR_PROJECT_SLUG" -ItemType Directory | Out-Null;
+	}
+
+	Copy-Item -Path * -Include "*.exe", "*.dll" -Destination "../deps/$ENV:APPVEYOR_PROJECT_SLUG";
+
+	Compress-Archive -Path ../deps/$ENV:APPVEYOR_PROJECT_SLUG/* -CompressionLevel Optimal -DestinationPath "../deps/$($ENV:APPVEYOR_PROJECT_SLUG)-v$($ENV:APPVEYOR_BUILD_VERSION).zip";
+
+	Remove-Item -Path ../deps/$ENV:APPVEYOR_PROJECT_SLUG -Force -Recurse;
+} else {
+	# Do not assign a release number or deploy
+  $env:CI_DEPLOY_GITHUB = $false;
+}
