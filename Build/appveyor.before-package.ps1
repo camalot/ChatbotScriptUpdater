5e7e503467190cc96e6d547bde70b0e0c5b6f2b7
Ryan Conrad <camalot@gmail.com>
2019-09-14 21:13:18 -0500

    artifacts

--- Build/appveyor.before-package.ps1
+++ Build/appveyor.before-package.ps1
@@ -6,11 +6,11 @@ if ( !$env:APPVEYOR_PULL_REQUEST_NUMBER -and ($env:APPVEYOR_REPO_BRANCH -eq "dev
 			New-Item -Path "$env:APPVEYOR_BUILD_FOLDER/deps/$ENV:APPVEYOR_PROJECT_SLUG" -ItemType Directory | Out-Null;
 	}
 	Write-Host "Copy * to $env:APPVEYOR_BUILD_FOLDER/deps/$ENV:APPVEYOR_PROJECT_SLUG";
-	Copy-Item -Path $env:APPVEYOR_BUILD_FOLDER/ChatbotScriptUpdater/bin/debug/* -Include "*.exe", "*.dll" -Destination "$env:APPVEYOR_BUILD_FOLDER/deps/$ENV:APPVEYOR_PROJECT_SLUG" | Write-Host;
+	Copy-Item -Path $env:APPVEYOR_BUILD_FOLDER/ChatbotScriptUpdater/bin/debug/* -Include "*.exe", "*.dll" -Destination "$env:APPVEYOR_BUILD_FOLDER/deps/$ENV:APPVEYOR_PROJECT_SLUG/" | Write-Host;
 
 	Compress-Archive -LiteralPath $env:APPVEYOR_BUILD_FOLDER/deps/$ENV:APPVEYOR_PROJECT_SLUG/* -CompressionLevel Optimal -DestinationPath "$env:APPVEYOR_BUILD_FOLDER/deps/$($ENV:APPVEYOR_PROJECT_SLUG)-v$($ENV:APPVEYOR_BUILD_VERSION).zip";
 
-	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER/deps/$ENV:APPVEYOR_PROJECT_SLUG/ | Write-Host;
+	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER/deps/ | Write-Host;
 
 	Remove-Item -Path $env:APPVEYOR_BUILD_FOLDER/deps/$ENV:APPVEYOR_PROJECT_SLUG -Force -Recurse;
 } else {
