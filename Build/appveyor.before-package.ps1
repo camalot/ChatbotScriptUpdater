8b184cae3ab38741c11b3fb920a73a380b30c10c
Ryan Conrad <camalot@gmail.com>
2019-09-14 20:57:02 -0500

    debug

--- Build/appveyor.before-package.ps1
+++ Build/appveyor.before-package.ps1
@@ -1,13 +1,16 @@
 if ( !$env:APPVEYOR_PULL_REQUEST_NUMBER -and ($env:APPVEYOR_REPO_BRANCH -eq "develop") ) {
-  $env:CI_DEPLOY_GITHUB = $true;
+  $env:CI_DEPLOY_GITHUB = $false;
 
 	if ( !(Test-Path -Path "../deps/$ENV:APPVEYOR_PROJECT_SLUG" ) ) {
+			Write-Host "Create ../deps/$ENV:APPVEYOR_PROJECT_SLUG";
 			New-Item -Path "../deps/$ENV:APPVEYOR_PROJECT_SLUG" -ItemType Directory | Out-Null;
 	}
-
+	Write-Host "Copy * to ../deps/$ENV:APPVEYOR_PROJECT_SLUG";
 	Copy-Item -Path * -Include "*.exe", "*.dll" -Destination "../deps/$ENV:APPVEYOR_PROJECT_SLUG";
 
 	Compress-Archive -Path ../deps/$ENV:APPVEYOR_PROJECT_SLUG/* -CompressionLevel Optimal -DestinationPath "../deps/$($ENV:APPVEYOR_PROJECT_SLUG)-v$($ENV:APPVEYOR_BUILD_VERSION).zip";
+
+	Get-ChildItem -Path ../deps/$ENV:APPVEYOR_PROJECT_SLUG/ | Write-Host;
 
 	Remove-Item -Path ../deps/$ENV:APPVEYOR_PROJECT_SLUG -Force -Recurse;
 } else {
